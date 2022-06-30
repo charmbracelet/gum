d232c84ef85fcb0689bbbc52eff3ecaf6565a88d
Maas Lalani <maas@lalani.dev>
2022-06-30 16:15:30 -0400

    feat: `package spin`
    Package spin provides a shell script interface for the spinner bubble.
    https://github.com/charmbracelet/bubbles/spinner
    
    It is useful for displaying that some task is running in the background
    while consuming it's output so that it is not shown to the user.
    
    For example, let's do a long running task:
    ```
    $ sleep 5
    ```
    
    We can simply prepend a spinner to this task to show it to the user,
    while performing the task / command in the background.
    
    ```
    $ sea spin -t "Taking a nap..." -- sleep 5
    ```
    
    The spinner will automatically exit when the task is complete.

--- spin/main.go
+++ spin/main.go
@@ -1,30 +1,66 @@
+// Package spin provides a shell script interface for the spinner bubble.
+// https://github.com/charmbracelet/bubbles/spinner
+//
+// It is useful for displaying that some task is running in the background
+// while consuming it's output so that it is not shown to the user.
+//
+// For example, let's do a long running task:
+//   $ sleep 5
+//
+// We can simply prepend a spinner to this task to show it to the user,
+// while performing the task / command in the background.
+//
+//   $ sea spin -t "Taking a nap..." -- sleep 5
+//
+// The spinner will automatically exit when the task is complete.
 package spin
 
 import (
-	"strings"
+	"os/exec"
 
 	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/muesli/coral"
 )
 
+// model is the state of the spinner program.
+// it tracks which spinner to use, the title to show the user, and the command to run in the background.
 type model struct {
 	spinner spinner.Model
 	title   string
-	command string
+	command []string
+}
+
+type finishCommandMsg struct{ output string }
+
+func commandStart(command []string) tea.Cmd {
+	return func() tea.Msg {
+		var args []string
+		if len(command) > 1 {
+			args = command[1:]
+		}
+		out, _ := exec.Command(command[0], args...).Output()
+		return finishCommandMsg{output: string(out)}
+	}
 }
 
 func (m model) Init() tea.Cmd {
-	return m.spinner.Tick
+	return tea.Batch(
+		m.spinner.Tick,
+		commandStart(m.command),
+	)
 }
 func (m model) View() string { return m.spinner.View() + " " + m.title }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
+	case finishCommandMsg:
+		return m, tea.Quit
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "esc", "enter":
+		case "ctrl+c":
 			return m, tea.Quit
 		}
 	}
@@ -34,8 +70,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 type options struct {
-	frames *string
-	title  *string
+	spinner *string
+	title   *string
+	color   *string
 }
 
 // Cmd returns the spin command
@@ -48,11 +85,12 @@ func Cmd() *coral.Command {
 		Args:  coral.ArbitraryArgs,
 		RunE: func(cmd *coral.Command, args []string) error {
 			s := spinner.New()
-			s.Spinner = spinners[*opts.frames]
+			s.Spinner = spinners[*opts.spinner]
+			s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color(*opts.color))
 			m := model{
 				spinner: s,
 				title:   *opts.title,
-				command: strings.Join(args, " "),
+				command: args,
 			}
 			p := tea.NewProgram(m)
 			_, err := p.StartReturningModel()
@@ -63,8 +101,9 @@ func Cmd() *coral.Command {
 	f := cmd.Flags()
 
 	opts = options{
-		frames: f.StringP("frames", "f", "dot", "The type of spinner to use"),
-		title:  f.StringP("title", "t", "Loading", "The title of the action being performed, shown to the right of the spinner"),
+		spinner: f.StringP("spinner", "s", "dot", "The type of spinner to use (line, dot, minidot, jump, pulse, points, globe, moon, monkey, meter, hamburger)"),
+		title:   f.StringP("title", "t", "Loading", "The title of the action being performed, shown to the right of the spinner"),
+		color:   f.StringP("color", "c", "white", "The color of the spinner"),
 	}
 
 	return cmd
