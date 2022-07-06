ca09fab1919ee07786bd279d93216c5a08a56c09
Maas Lalani <maas@lalani.dev>
2022-07-05 22:59:21 -0400

    refactor: use `alecthomas/kong`

--- go.mod
+++ go.mod
@@ -1,29 +1,5 @@
-module github.com/charmbracelet/seashell
+module github.com/charmbracelet/sodapop
 
 go 1.18
 
-require (
-	github.com/charmbracelet/bubbles v0.12.1-0.20220628162924-2fd583c8ef43
-	github.com/charmbracelet/bubbletea v0.22.1-0.20220628193422-db4f6474c9c2
-	github.com/charmbracelet/lipgloss v0.5.1-0.20220407020210-a86f21a0ae43
-	github.com/muesli/coral v1.0.0
-)
-
-require (
-	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
-	github.com/containerd/console v1.0.3 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
-	github.com/muesli/cancelreader v0.2.1 // indirect
-	github.com/muesli/reflow v0.3.0 // indirect
-	github.com/muesli/termenv v0.12.1-0.20220606102431-9500d1649068 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/sahilm/fuzzy v0.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-)
+require github.com/alecthomas/kong v0.6.1
--- main.go
+++ main.go
@@ -1,35 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/charmbracelet/seashell/input"
-	"github.com/charmbracelet/seashell/search"
-	"github.com/charmbracelet/seashell/spin"
-	"github.com/charmbracelet/seashell/style"
-	"github.com/muesli/coral"
+	"github.com/alecthomas/kong"
 )
 
-var rootCmd = &coral.Command{
-	Use:   "sea",
-	Short: "Sea Shell is a shell integration for bubbles and lipgloss for input and layout management",
-	Long:  "Sea Shell is a wrapper for using Bubbles components\nand Lip Gloss layouts in the terminal directly in shell scripts",
+// Seashell is the top-level command.
+var Seashell struct {
+	Input struct {
+		Prompt      string `help:"Prompt to display" default:"> "`
+		Placeholder string `help:"Placeholder value" default:"..."`
+		Width       int    `help:"Input width" default:"20"`
+	} `cmd:"" help:"Prompt for input."`
+
+	Search struct {
+		Prompt      string `help:"Prompt to display" default:"> "`
+		Placeholder string `help:"Placeholder value" default:"..."`
+		Width       int    `help:"Input width" default:"20"`
+		AccentColor string `help:"Accent color for prompt, indicator, and matches." default:"#FF06B7"`
+		Indicator   string `help:"Character to use to indicate the selected matches" default:"•"`
+	} `cmd:"" help:"Fuzzy search options."`
+
+	Spin struct {
+		Spinner string `help:"Type of spinner to use" enum:"line,dot,minidot,jump,pulse,points,globe,moon,monkey,meter,hamburger" default:"dots"`
+		Title   string `help:"Action being performed" default:"Loading..."`
+		Color   string `help:"Color to use for the spinner" default:"#FF06B7"`
+	} `cmd:"" help:"Show spinner while executing a command."`
+
+	Style struct {
+		Background       string `help:"The background color to apply"`
+		Foreground       string `help:"The foreground color to apply"`
+		BorderBackground string `help:"The border background color to apply"`
+		BorderForeground string `help:"The border foreground color to apply"`
+		Align            string `help:"The text alignment (left, center, right, bottom, middle, top)"`
+		Border           string `help:"The border style to apply (none, hidden, normal, rounded, thick, double)"`
+		Height           int    `help:"The height the output should take up"`
+		Width            int    `help:"The width the output should take up"`
+		Margin           string `help:"Margin to apply around the text."`
+		Padding          string `help:"Padding to apply around the text."`
+		Bold             bool   `help:"Whether to apply bold formatting"`
+		Faint            bool   `help:"Whether to apply faint formatting"`
+		Italic           bool   `help:"Whether to apply italic formatting"`
+		Strikethrough    bool   `help:"Whether to apply strikethrough formatting"`
+	} `cmd:"" help:"Style some text."`
+
+	Layout struct {
+	} `cmd:"" help:"Layout some text."`
 }
 
-// Execute executes the root command
 func main() {
-	rootCmd.AddCommand(input.Cmd())
-	rootCmd.AddCommand(search.Cmd())
-	rootCmd.AddCommand(spin.Cmd())
-	rootCmd.AddCommand(style.Cmd())
-
-	// We re-add a help command to remove the shorthand, so that we can use the
-	// -h shorthand for height
-	rootCmd.PersistentFlags().BoolP("help", "", false, "Show help for a command")
-
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	ctx := kong.Parse(&Seashell)
+	switch ctx.Command() {
+	case "input":
+	case "search":
+	case "spin":
+	case "style":
+		fmt.Println(Seashell)
+	case "layout":
+	default:
 	}
 }
