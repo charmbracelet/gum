ff1f02f47062d8f71e84f09c3189573f4a4cc931
Maas Lalani <maas@lalani.dev>
2022-07-07 22:39:42 -0400

    fix(write): Placeholder color fix

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/alecthomas/kong v0.6.1
-	github.com/charmbracelet/bubbles v0.13.0
+	github.com/charmbracelet/bubbles v0.13.1-0.20220708023448-746ec595c3d1
 	github.com/charmbracelet/bubbletea v0.22.0
 	github.com/charmbracelet/lipgloss v0.5.0
 	github.com/mattn/go-runewidth v0.0.13
--- write/options.go
+++ write/options.go
@@ -2,11 +2,11 @@ package write
 
 // Options are the customization options for the textarea.
 type Options struct {
-	CursorColor     string `help:"Color for cursor" default:"212"`
+	CursorColor     string `help:"Cursor color" default:"212"`
 	Height          int    `help:"Text area height" default:"5"`
 	Placeholder     string `help:"Placeholder value" default:"Write something..."`
 	Prompt          string `help:"Prompt to display" default:"┃ "`
-	PromptColor     string `help:"Color for prompt" default:"238"`
+	PromptColor     string `help:"Prompt color" default:"238"`
 	ShowCursorLine  bool   `help:"Show cursor line" default:"false"`
 	ShowLineNumbers bool   `help:"Show line numbers" default:"false"`
 	Width           int    `help:"Text area width" default:"50"`
