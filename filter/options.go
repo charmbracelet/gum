ba0651db089329a8317bef4dacbca10cada5c755
Christian Rocha <christian@rocha.is>
2022-07-26 10:23:03 -0700

    chore: change filter default placeholder text to 'Filter...'

--- filter/options.go
+++ filter/options.go
@@ -8,7 +8,7 @@ type Options struct {
 	IndicatorStyle style.Styles `embed:"" prefix:"indicator." set:"defaultForeground=212" set:"name=indicator"`
 	TextStyle      style.Styles `embed:"" prefix:"text."`
 	MatchStyle     style.Styles `embed:"" prefix:"match." set:"defaultForeground=212" set:"name=matched text"`
-	Placeholder    string       `help:"Placeholder value" default:"Search..."`
+	Placeholder    string       `help:"Placeholder value" default:"Filter..."`
 	Prompt         string       `help:"Prompt to display" default:"> "`
 	PromptStyle    style.Styles `embed:"" prefix:"prompt." set:"defaultForeground=240" set:"name=prompt"`
 	Width          int          `help:"Input width" default:"20"`
