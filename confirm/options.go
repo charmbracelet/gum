cec8ecd7823bd401b3c8ec57cb43300d2252130b
Maas Lalani <maas@lalani.dev>
2022-07-26 17:53:47 -0400

    fix(confirm): slightly change default colors

--- confirm/options.go
+++ confirm/options.go
@@ -8,6 +8,6 @@ type Options struct {
 	Negative        string       `help:"The title of the negative action" default:"No"`
 	Prompt          string       `arg:"" help:"Prompt to display." default:"Are you sure?"`
 	PromptStyle     style.Styles `embed:"" prefix:"prompt." help:"The style of the prompt" set:"defaultMargin=1 0 0 0"`
-	UnselectedStyle style.Styles `embed:"" prefix:"unselected." help:"The style of the unselected action" set:"defaultBackground=237" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
-	SelectedStyle   style.Styles `embed:"" prefix:"selected." help:"The style of the selected action" set:"defaultBackground=240" set:"defaultForeground=212" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
+	UnselectedStyle style.Styles `embed:"" prefix:"unselected." help:"The style of the unselected action" set:"defaultBackground=235" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
+	SelectedStyle   style.Styles `embed:"" prefix:"selected." help:"The style of the selected action" set:"defaultBackground=236" set:"defaultForeground=212" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
 }
