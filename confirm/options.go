0bf8091d5e773a67960d7b9c3f621e9de430db34
Christian Rocha <christian@rocha.is>
2022-07-27 12:38:28 -0700

    chore(confirm): increase contrast on selected choice

--- confirm/options.go
+++ confirm/options.go
@@ -9,5 +9,5 @@ type Options struct {
 	Prompt          string       `arg:"" help:"Prompt to display." default:"Are you sure?"`
 	PromptStyle     style.Styles `embed:"" prefix:"prompt." help:"The style of the prompt" set:"defaultMargin=1 0 0 0"`
 	UnselectedStyle style.Styles `embed:"" prefix:"unselected." help:"The style of the unselected action" set:"defaultBackground=235" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
-	SelectedStyle   style.Styles `embed:"" prefix:"selected." help:"The style of the selected action" set:"defaultBackground=236" set:"defaultForeground=212" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
+	SelectedStyle   style.Styles `embed:"" prefix:"selected." help:"The style of the selected action" set:"defaultBackground=212" set:"defaultForeground=230" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
 }
