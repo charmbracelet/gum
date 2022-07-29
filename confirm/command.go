eb12ef442bd50cf14d0359070e7bb1a8025717e5
tim <tim@timdev.de>
2022-07-29 17:28:59 +0200

    fix(confirm): fix #31

--- confirm/command.go
+++ confirm/command.go
@@ -9,10 +9,16 @@ import (
 // Run provides a shell script interface for prompting a user to confirm an
 // action with an affirmative or negative answer.
 func (o Options) Run() error {
+	var selected int
+	if o.NegativeDefault {
+		selected = 1
+	} else {
+		selected = 0
+	}
 	m, err := tea.NewProgram(model{
 		affirmative:     o.Affirmative,
 		negative:        o.Negative,
-		selected:        0,
+		selected:        selected,
 		prompt:          o.Prompt,
 		selectedStyle:   o.SelectedStyle.ToLipgloss(),
 		unselectedStyle: o.UnselectedStyle.ToLipgloss(),
--- confirm/options.go
+++ confirm/options.go
@@ -7,6 +7,7 @@ type Options struct {
 	Affirmative     string       `help:"The title of the affirmative action" default:"Yes"`
 	Negative        string       `help:"The title of the negative action" default:"No"`
 	Prompt          string       `arg:"" help:"Prompt to display." default:"Are you sure?"`
+	NegativeDefault bool         `short:"n" help:"Default to negative option"`
 	PromptStyle     style.Styles `embed:"" prefix:"prompt." help:"The style of the prompt" set:"defaultMargin=1 0 0 0"`
 	UnselectedStyle style.Styles `embed:"" prefix:"unselected." help:"The style of the unselected action" set:"defaultBackground=235" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
 	SelectedStyle   style.Styles `embed:"" prefix:"selected." help:"The style of the selected action" set:"defaultBackground=212" set:"defaultForeground=230" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
