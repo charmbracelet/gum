56e54467cbb577a951033519320350fd270516f2
Maas Lalani <maas@lalani.dev>
2022-07-30 18:12:51 -0400

    docs: add filter example with identification (key / value)

--- examples/filter-key-value.sh
+++ examples/filter-key-value.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-cat <<EOF | gum filter
+export LIST=$(cat <<END
 Cow:Moo
 Cat:Meow
 Dog:Woof
-EOF
+END)
+
+ANIMAL=$(echo "$LIST" | cut -d':' -f1 | gum filter)
+SOUND=$(echo "$LIST" | grep $ANIMAL | cut -d':' -f2)
+
+echo "The $ANIMAL goes $SOUND"
