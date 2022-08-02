b850fb26a2b16a58fca461b7a54bab0afa68a708
Christian Muehlhaeuser <muesli@gmail.com>
2022-08-02 17:17:36 +0200

    chore: move git example into examples dir

--- examples/basic-git-cli.sh
+++ examples/basic-git-cli.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# A basic cli for handling the staging with git
+#
+# This is a simple cli that utilizes the output from several git commands, and
+# pipes them into `gum filter` and `gum choose` to provide a simple cli wrapper
+# for `git add` and `git reset`.
+#
+# For a more feature complete version of this script check out
+# [Nickiel12's Git-Gum](https://github.com/Nickiel12/Git-Gum).
+
 git_add_filter(){
     input=$1
     if [[ $input == "M "* ]]; then
