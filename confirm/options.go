7842b83c9df638123b04d4267944416925bfa0f2
Maas Lalani <maas@lalani.dev>
2022-08-03 14:06:55 -0400

    fix(confirm): readable on light theme

--- confirm/options.go
+++ confirm/options.go
@@ -10,6 +10,6 @@ type Options struct {
 	Default         bool         `help:"Default confirmation action" default:"true"`
 	Prompt          string       `arg:"" help:"Prompt to display." default:"Are you sure?"`
 	PromptStyle     style.Styles `embed:"" prefix:"prompt." help:"The style of the prompt" set:"defaultMargin=1 0 0 0"`
-	UnselectedStyle style.Styles `embed:"" prefix:"unselected." help:"The style of the unselected action" set:"defaultBackground=235" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
+	UnselectedStyle style.Styles `embed:"" prefix:"unselected." help:"The style of the unselected action" set:"defaultBackground=235" set:"defaultForeground=254" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
 	SelectedStyle   style.Styles `embed:"" prefix:"selected." help:"The style of the selected action" set:"defaultBackground=212" set:"defaultForeground=230" set:"defaultPadding=0 3" set:"defaultMargin=1 1"`
 }
