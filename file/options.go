7756c809d1146ff97817bc1ace6f8a4d094add9c
Maas Lalani <maas@lalani.dev>
2022-12-13 15:36:58 -0500

    fix(file): all default false (to match ls), GUM_FILE_ environment

--- file/options.go
+++ file/options.go
@@ -5,14 +5,14 @@ import "github.com/charmbracelet/gum/style"
 // Options are the options for the file command.
 type Options struct {
 	// Path is the path to the folder / directory to begin traversing.
-	Path string `arg:"" optional:"" name:"path" help:"The path to the folder to begin traversing"`
+	Path string `arg:"" optional:"" name:"path" help:"The path to the folder to begin traversing" env:"GUM_FILE_PATH"`
 	// Cursor is the character to display in front of the current selected items.
-	Cursor    string `short:"c" help:"The cursor character" default:">"`
-	All       bool   `short:"a" help:"Show hidden and 'dot' files" default:"true"`
-	File      bool   `help:"Allow files selection" default:"true"`
-	Directory bool   `help:"Allow directories selection" default:"false"`
+	Cursor    string `short:"c" help:"The cursor character" default:">" env:"GUM_FILE_CURSOR"`
+	All       bool   `short:"a" help:"Show hidden and 'dot' files" default:"false" env:"GUM_FILE_ALL"`
+	File      bool   `help:"Allow files selection" default:"true" env:"GUM_FILE_FILE"`
+	Directory bool   `help:"Allow directories selection" default:"false" env:"GUM_FILE_DIRECTORY"`
 
-	Height           int          `help:"Maximum number of files to display" default:"0"`
+	Height           int          `help:"Maximum number of files to display" default:"0" env:"GUM_FILE_HEIGHT"`
 	CursorStyle      style.Styles `embed:"" prefix:"cursor." help:"The cursor style" set:"defaultForeground=212" envprefix:"GUM_FILE_CURSOR_"`
 	SymlinkStyle     style.Styles `embed:"" prefix:"symlink." help:"The style to use for symlinks" set:"defaultForeground=36" envprefix:"GUM_FILE_SYMLINK_"`
 	DirectoryStyle   style.Styles `embed:"" prefix:"directory." help:"The style to use for directories" set:"defaultForeground=99" envprefix:"GUM_FILE_DIRECTORY_"`
