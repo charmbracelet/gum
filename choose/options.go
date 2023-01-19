fc991df82790ad9d9282456372733f81661de0fe
Dieter Eickstaedt <eickstaedt@deicon.de>
2023-01-19 19:25:41 +0100

    merge main

--- choose/options.go
+++ choose/options.go
@@ -11,6 +11,7 @@ type Options struct {
 
 	Limit             int           `help:"Maximum number of options to pick" default:"1" group:"Selection"`
 	NoLimit           bool          `help:"Pick unlimited number of options (ignores limit)" group:"Selection"`
+	Ordered           bool          `help:"Maintain the order of the selected options" env:"GUM_CHOOSE_ORDERED"`
 	Height            int           `help:"Height of the list" default:"10" env:"GUM_CHOOSE_HEIGHT"`
 	Cursor            string        `help:"Prefix to show on item that corresponds to the cursor position" default:"> " env:"GUM_CHOOSE_CURSOR"`
 	CursorPrefix      string        `help:"Prefix to show on the cursor item (hidden if limit is 1)" default:"○ " env:"GUM_CHOOSE_CURSOR_PREFIX"`
