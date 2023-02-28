07dd11fe45ae0d4172933831a189d692f1844195
Maas Lalani <maas@lalani.dev>
2023-02-27 19:18:12 -0500

    fix: stage all confirmation

--- examples/commit.sh
+++ examples/commit.sh
@@ -10,6 +10,10 @@
 #
 # alias gcm='git commit -m "$(gum input)" -m "$(gum write)"'
 
+if [ -z "$(git status -s -uno | grep -v '^ ' | awk '{print $2}')" ]; then
+    gum confirm "Stage all?" && git add .
+fi
+
 TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
 SCOPE=$(gum input --placeholder "scope")
 
@@ -19,12 +23,6 @@ test -n "$SCOPE" && SCOPE="($SCOPE)"
 # Pre-populate the input with the type(scope): so that the user may change it
 SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
 DESCRIPTION=$(gum write --placeholder "Details of this change")
-
-# Check if any staged files are present. If there are non, ask the user if 
-# they want to commit all files
-if [ -z "$(git status -s -uno | grep -v '^ ' | awk '{print $2}')" ]; then
-    gum confirm "No staged files. Commit all?" && git add .
-fi
 
 # Commit these changes if user confirms
 gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
