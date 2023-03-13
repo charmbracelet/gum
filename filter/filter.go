f8f8b7a97ab9735f84748f83617de934af083938
mikael <34864484+MikaelFangel@users.noreply.github.com>
2023-03-13 23:30:07 +0100

    added header for filter

--- filter/filter.go
+++ filter/filter.go
@@ -27,6 +27,7 @@ type model struct {
 	choices               []string
 	matches               []fuzzy.Match
 	cursor                int
+	header                string
 	selected              map[string]struct{}
 	limit                 int
 	numSelected           int
@@ -36,6 +37,7 @@ type model struct {
 	height                int
 	aborted               bool
 	quitting              bool
+	headerStyle           lipgloss.Style
 	matchStyle            lipgloss.Style
 	textStyle             lipgloss.Style
 	indicatorStyle        lipgloss.Style
@@ -121,10 +123,22 @@ func (m model) View() string {
 	m.viewport.SetContent(s.String())
 
 	// View the input and the filtered choices
+	var view string
 	if m.reverse {
-		return m.viewport.View() + "\n" + m.textinput.View()
+		view = m.viewport.View() + "\n" + m.textinput.View()
+	} else {
+		view = m.textinput.View() + "\n" + m.viewport.View()
 	}
-	return m.textinput.View() + "\n" + m.viewport.View()
+
+	if m.header != "" {
+		header := m.headerStyle.Render(m.header)
+		if m.reverse {
+			view = lipgloss.JoinVertical(lipgloss.Left, view, header)
+		}
+		view = lipgloss.JoinVertical(lipgloss.Left, header, view)
+	}
+
+	return view
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -133,6 +147,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		if m.height == 0 || m.height > msg.Height {
 			m.viewport.Height = msg.Height - lipgloss.Height(m.textinput.View())
+		}
+
+		// Make place in the view port if header is set
+		if m.header != "" {
+			m.viewport.Height = m.viewport.Height - lipgloss.Height(m.header)
 		}
 		m.viewport.Width = msg.Width
 		if m.reverse {
--- filter/options.go
+++ filter/options.go
@@ -13,6 +13,8 @@ type Options struct {
 	SelectedPrefixStyle   style.Styles `embed:"" prefix:"selected-indicator." set:"defaultForeground=212" envprefix:"GUM_FILTER_SELECTED_PREFIX_"`
 	UnselectedPrefix      string       `help:"Character to indicate unselected items (hidden if limit is 1)" default:" ○ " env:"GUM_FILTER_UNSELECTED_PREFIX"`
 	UnselectedPrefixStyle style.Styles `embed:"" prefix:"unselected-prefix." set:"defaultForeground=240" envprefix:"GUM_FILTER_UNSELECTED_PREFIX_"`
+	HeaderStyle           style.Styles `embed:"" prefix:"header." set:"defaultForeground=240" envprefix:"GUM_FILTER_HEADER_"`
+	Header                string       `help:"Header value" default:"" env:"GUM_FILTER_HEADER"`
 	TextStyle             style.Styles `embed:"" prefix:"text." envprefix:"GUM_FILTER_TEXT_"`
 	MatchStyle            style.Styles `embed:"" prefix:"match." set:"defaultForeground=212" envprefix:"GUM_FILTER_MATCH_"`
 	Placeholder           string       `help:"Placeholder value" default:"Filter..." env:"GUM_FILTER_PLACEHOLDER"`
