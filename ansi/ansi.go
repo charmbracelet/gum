8da9620bfd15eef5b55cfec42572a990b952c056
Maas Lalani <maas@lalani.dev>
2023-04-12 09:57:36 -0400

    chore(lint): add function comment

--- ansi/ansi.go
+++ ansi/ansi.go
@@ -4,6 +4,7 @@ import "regexp"
 
 var ansiEscape = regexp.MustCompile(`\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])`)
 
+// Strip strips a string of any of it's ansi sequences.
 func Strip(text string) string {
 	return ansiEscape.ReplaceAllString(text, "")
 }
