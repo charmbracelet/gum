c668e153e6244d4e7e208980dc0c5c545819031d
Maas Lalani <maas@lalani.dev>
2023-05-11 23:43:52 -0400

    feat(format): add GUM_FORMAT_THEME / GUM_FORMAT_LANGUAGE environment variable configuration

--- format/options.go
+++ format/options.go
@@ -3,8 +3,8 @@ package format
 // Options is customization options for the format command.
 type Options struct {
 	Template []string `arg:"" optional:"" help:"Template string to format (can also be provided via stdin)"`
-	Theme    string   `help:"Glamour theme to use for markdown formatting" default:"pink"`
-	Language string   `help:"Programming language to parse code" short:"l" default:""`
+	Theme    string   `help:"Glamour theme to use for markdown formatting" default:"pink" env:"GUM_FORMAT_THEME"`
+	Language string   `help:"Programming language to parse code" short:"l" default:"" env:"GUM_FORMAT_LANGUAGE"`
 
-	Type string `help:"Format to use (markdown,template,code,emoji)" enum:"markdown,template,code,emoji" short:"t" default:"markdown"`
+	Type string `help:"Format to use (markdown,template,code,emoji)" enum:"markdown,template,code,emoji" short:"t" default:"markdown" env:"GUM_FORMAT_TYPE"`
 }
