ea6bf88e2d7dd444551fc37c0f5d03dc238b6e70
Maas Lalani <maas@lalani.dev>
2023-06-05 16:05:22 -0400

    feat(write,input): add `--cursor.mode=hide,blink,static`

--- input/options.go
+++ input/options.go
@@ -8,6 +8,7 @@ type Options struct {
 	Prompt      string       `help:"Prompt to display" default:"> " env:"GUM_INPUT_PROMPT"`
 	PromptStyle style.Styles `embed:"" prefix:"prompt." envprefix:"GUM_INPUT_PROMPT_"`
 	CursorStyle style.Styles `embed:"" prefix:"cursor." set:"defaultForeground=212" envprefix:"GUM_INPUT_CURSOR_"`
+	CursorMode  string       `prefix:"cursor." name:"mode" help:"Cursor mode" default:"blink" enum:"blink,hide,static" env:"GUM_INPUT_CURSOR_MODE"`
 	Value       string       `help:"Initial value (can also be passed via stdin)" default:""`
 	CharLimit   int          `help:"Maximum value length (0 for no limit)" default:"400"`
 	Width       int          `help:"Input width (0 for terminal width)" default:"40" env:"GUM_INPUT_WIDTH"`
