77bf7f2b23cf93f25b0c812a149418b2583e2e6c
Jelle Besseling <jelle@bigbridge.nl>
2023-08-14 09:20:54 +0200

    Update spin/options.go
    Co-authored-by: Maas Lalani <maas@lalani.dev>

--- spin/options.go
+++ spin/options.go
@@ -11,7 +11,7 @@ type Options struct {
 	Command []string `arg:"" help:"Command to run"`
 
 	ShowOutput    bool         `help:"Show output of command" default:"false" env:"GUM_SPIN_SHOW_OUTPUT"`
-	OutputOnError bool         `help:"Show output of command if it fails" default:"false" env:"GUM_SPIN_OUTPUT_ON_ERROR"`
+	ShowError bool         `help:"Show output (error) of command if it fails" default:"false" env:"GUM_SPIN_SHOW_ERROR"`
 	Spinner       string       `help:"Spinner type" short:"s" type:"spinner" enum:"line,dot,minidot,jump,pulse,points,globe,moon,monkey,meter,hamburger" default:"dot" env:"GUM_SPIN_SPINNER"`
 	SpinnerStyle  style.Styles `embed:"" prefix:"spinner." set:"defaultForeground=212" envprefix:"GUM_SPIN_SPINNER_"`
 	Title         string       `help:"Text to display to user while spinning" default:"Loading..." env:"GUM_SPIN_TITLE"`
