fb31a20945d29befbbb21b9a4a993839f2a9664f
Jelle Besseling <jelle@bigbridge.nl>
2023-08-14 09:20:58 +0200

    Update spin/options.go
    Co-authored-by: Maas Lalani <maas@lalani.dev>

--- spin/options.go
+++ spin/options.go
@@ -17,5 +17,5 @@ type Options struct {
 	Title         string       `help:"Text to display to user while spinning" default:"Loading..." env:"GUM_SPIN_TITLE"`
 	TitleStyle    style.Styles `embed:"" prefix:"title." envprefix:"GUM_SPIN_TITLE_"`
 	Align         string       `help:"Alignment of spinner with regard to the title" short:"a" type:"align" enum:"left,right" default:"left" env:"GUM_SPIN_ALIGN"`
-  Timeout      time.Duration `help:"Timeout until spin command aborts" default:"0" env:"GUM_SPIN_TIMEOUT"`
+    Timeout      time.Duration `help:"Timeout until spin command aborts" default:"0" env:"GUM_SPIN_TIMEOUT"`
 }
