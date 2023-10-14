3447bb04ba251b2331b8fa74bc6d0608246b9195
Elliot Courant <me@elliotcourant.dev>
2023-10-13 19:57:47 -0500

    feat(spin): Add support for `--show-error` for the spinner.
    This makes it so that if the `--show-error` flag is provided then the
    full output of the command will be printed if the command fails. This
    kind of works in conjuncture with `--show-output` in that if the command
    succeeds only STDOUT is pushed. If the command fails both `STDOUT` and
    `STDERR` are pushed.
    
    This builds off of https://github.com/charmbracelet/gum/pull/371
    
    Resolves #55

--- spin/command.go
+++ spin/command.go
@@ -44,14 +44,25 @@ func (o Options) Run() error {
 		return fmt.Errorf("failed to access stdout: %w", err)
 	}
 
-	if o.ShowOutput {
-		// BubbleTea writes the View() to stderr.
-		// If the program is being piped then put the accumulated output in stdout.
-		if !isTTY {
-			_, err := os.Stdout.WriteString(m.stdout)
-			if err != nil {
-				return fmt.Errorf("failed to write to stdout: %w", err)
+	// If the command succeed, and we are printing output and we are in a TTY then push the STDOUT we got
+	// to the actual STDOUT for piping or other things.
+	if m.status == 0 {
+		if o.ShowOutput {
+			// BubbleTea writes the View() to stderr.
+			// If the program is being piped then put the accumulated output in stdout.
+			if !isTTY {
+				_, err := os.Stdout.WriteString(m.stdout)
+				if err != nil {
+					return fmt.Errorf("failed to write to stdout: %w", err)
+				}
 			}
+		}
+	} else if o.ShowError {
+		// Otherwise if we are showing errors and the command did not exit with a 0 status code then push all of the command
+		// output to the terminal. This way failed commands can be debugged.
+		_, err := os.Stdout.WriteString(m.output)
+		if err != nil {
+			return fmt.Errorf("failed to write to stdout: %w", err)
 		}
 	}
 
