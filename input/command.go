7bc85c91bc7a726740dfc8c11d58d098ba25716a
Maas Lalani <maas@lalani.dev>
2024-01-25 17:04:02 -0500

    feat: use huh for gum input

--- input/command.go
+++ input/command.go
@@ -2,60 +2,47 @@ package input
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/charmbracelet/bubbles/textinput"
-	tea "github.com/charmbracelet/bubbletea"
-
-	"github.com/charmbracelet/gum/cursor"
-	"github.com/charmbracelet/gum/internal/exit"
 	"github.com/charmbracelet/gum/internal/stdin"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
 )
 
 // Run provides a shell script interface for the text input bubble.
 // https://github.com/charmbracelet/bubbles/textinput
 func (o Options) Run() error {
-	i := textinput.New()
-	if o.Value != "" {
-		i.SetValue(o.Value)
-	} else if in, _ := stdin.Read(); in != "" {
-		i.SetValue(in)
+	if o.Value == "" {
+		o.Value, _ = stdin.Read()
 	}
 
-	i.Focus()
-	i.Prompt = o.Prompt
-	i.Placeholder = o.Placeholder
-	i.Width = o.Width
-	i.PromptStyle = o.PromptStyle.ToLipgloss()
-	i.PlaceholderStyle = o.PlaceholderStyle.ToLipgloss()
-	i.Cursor.Style = o.CursorStyle.ToLipgloss()
-	i.Cursor.SetMode(cursor.Modes[o.CursorMode])
-	i.CharLimit = o.CharLimit
+	theme := huh.ThemeCharm()
+	theme.Focused.Base.Border(lipgloss.Border{})
+	theme.Focused.Title = o.HeaderStyle.ToLipgloss()
+	theme.Focused.TextInput.Prompt = o.PromptStyle.ToLipgloss()
+	theme.Focused.TextInput.Placeholder = o.PlaceholderStyle.ToLipgloss()
+	theme.Focused.TextInput.Cursor = o.CursorStyle.ToLipgloss()
 
-	if o.Password {
-		i.EchoMode = textinput.EchoPassword
-		i.EchoCharacter = '•'
-	}
-
-	p := tea.NewProgram(model{
-		textinput:   i,
-		aborted:     false,
-		header:      o.Header,
-		headerStyle: o.HeaderStyle.ToLipgloss(),
-		timeout:     o.Timeout,
-		hasTimeout:  o.Timeout > 0,
-		autoWidth:   o.Width < 1,
-	}, tea.WithOutput(os.Stderr))
-	tm, err := p.Run()
+	err := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Password(o.Password).
+				Title(o.Header).
+				Prompt(o.Prompt).
+				CharLimit(o.CharLimit).
+				Placeholder(o.Placeholder).
+				Value(&o.Value),
+		),
+	).
+		WithWidth(o.Width).
+		WithShowHelp(false).
+		WithTheme(theme).
+		Run()
 	if err != nil {
-		return fmt.Errorf("failed to run input: %w", err)
-	}
-	m := tm.(model)
-
-	if m.aborted {
-		return exit.ErrAborted
+		return err
 	}
 
-	fmt.Println(m.textinput.Value())
+	if o.Value != "" {
+		fmt.Println(o.Value)
+	}
 	return nil
 }
