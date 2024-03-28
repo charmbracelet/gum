83b86c766173f9841c80b6ba88349692bb3e1f16
Maas Lalani <maas@lalani.dev>
2024-03-28 14:32:41 -0400

    feat: gum confirm with huh
    Use `huh` for `gum confirm`.

--- confirm/command.go
+++ confirm/command.go
@@ -4,38 +4,41 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/gum/internal/exit"
-
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
 )
 
 // Run provides a shell script interface for prompting a user to confirm an
 // action with an affirmative or negative answer.
 func (o Options) Run() error {
-	m, err := tea.NewProgram(model{
-		affirmative:      o.Affirmative,
-		negative:         o.Negative,
-		confirmation:     o.Default,
-		defaultSelection: o.Default,
-		timeout:          o.Timeout,
-		hasTimeout:       o.Timeout > 0,
-		prompt:           o.Prompt,
-		selectedStyle:    o.SelectedStyle.ToLipgloss(),
-		unselectedStyle:  o.UnselectedStyle.ToLipgloss(),
-		promptStyle:      o.PromptStyle.ToLipgloss(),
-	}, tea.WithOutput(os.Stderr)).Run()
+	theme := huh.ThemeCharm()
+	theme.Focused.Base = lipgloss.NewStyle().Margin(0, 1)
+	theme.Focused.Title = o.PromptStyle.ToLipgloss()
+	theme.Focused.FocusedButton = o.SelectedStyle.ToLipgloss()
+	theme.Focused.BlurredButton = o.UnselectedStyle.ToLipgloss()
+
+	var choice bool = o.Default
+
+	err := huh.NewForm(
+		huh.NewGroup(
+			huh.NewConfirm().
+				Affirmative(o.Affirmative).
+				Negative(o.Negative).
+				Title(o.Prompt).
+				Value(&choice),
+		),
+	).
+		WithTheme(theme).
+		WithShowHelp(false).
+		Run()
 
 	if err != nil {
 		return fmt.Errorf("unable to run confirm: %w", err)
 	}
 
-	if m.(model).aborted {
-		os.Exit(exit.StatusAborted)
-	} else if m.(model).confirmation {
-		os.Exit(0)
+	if !choice {
+		os.Exit(1)
 	}
-
-	os.Exit(1)
 
 	return nil
 }
