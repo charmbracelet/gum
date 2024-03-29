4d5d53169e89950000015994cd694ccc9e822f08
Maas Lalani <maas@lalani.dev>
2024-03-29 07:19:03 -0400

    feat: huh gum write (#525)

--- write/command.go
+++ write/command.go
@@ -2,15 +2,10 @@ package write
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
-	"github.com/charmbracelet/bubbles/textarea"
-	tea "github.com/charmbracelet/bubbletea"
-
-	"github.com/charmbracelet/gum/cursor"
-	"github.com/charmbracelet/gum/internal/exit"
 	"github.com/charmbracelet/gum/internal/stdin"
+	"github.com/charmbracelet/huh"
 )
 
 // Run provides a shell script interface for the text area bubble.
@@ -21,48 +16,33 @@ func (o Options) Run() error {
 		o.Value = strings.ReplaceAll(in, "\r", "")
 	}
 
-	a := textarea.New()
-	a.Focus()
+	var value = o.Value
 
-	a.Prompt = o.Prompt
-	a.Placeholder = o.Placeholder
-	a.ShowLineNumbers = o.ShowLineNumbers
-	a.CharLimit = o.CharLimit
+	theme := huh.ThemeCharm()
+	theme.Focused.Base = o.BaseStyle.ToLipgloss()
+	theme.Focused.TextInput.Cursor = o.CursorStyle.ToLipgloss()
+	theme.Focused.Title = o.HeaderStyle.ToLipgloss()
+	theme.Focused.TextInput.Placeholder = o.PlaceholderStyle.ToLipgloss()
+	theme.Focused.TextInput.Prompt = o.PromptStyle.ToLipgloss()
 
-	style := textarea.Style{
-		Base:             o.BaseStyle.ToLipgloss(),
-		Placeholder:      o.PlaceholderStyle.ToLipgloss(),
-		CursorLine:       o.CursorLineStyle.ToLipgloss(),
-		CursorLineNumber: o.CursorLineNumberStyle.ToLipgloss(),
-		EndOfBuffer:      o.EndOfBufferStyle.ToLipgloss(),
-		LineNumber:       o.LineNumberStyle.ToLipgloss(),
-		Prompt:           o.PromptStyle.ToLipgloss(),
-	}
+	err := huh.NewForm(
+		huh.NewGroup(
+			huh.NewText().
+				Title(o.Header).
+				Placeholder(o.Placeholder).
+				CharLimit(o.CharLimit).
+				ShowLineNumbers(o.ShowLineNumbers).
+				Value(&value),
+		),
+	).
+		WithWidth(o.Width).
+		WithHeight(o.Height).
+		WithShowHelp(false).Run()
 
-	a.BlurredStyle = style
-	a.FocusedStyle = style
-	a.Cursor.Style = o.CursorStyle.ToLipgloss()
-	a.Cursor.SetMode(cursor.Modes[o.CursorMode])
-
-	a.SetWidth(o.Width)
-	a.SetHeight(o.Height)
-	a.SetValue(o.Value)
-
-	p := tea.NewProgram(model{
-		textarea:    a,
-		header:      o.Header,
-		headerStyle: o.HeaderStyle.ToLipgloss(),
-		autoWidth:   o.Width < 1,
-	}, tea.WithOutput(os.Stderr))
-	tm, err := p.Run()
 	if err != nil {
-		return fmt.Errorf("failed to run write: %w", err)
-	}
-	m := tm.(model)
-	if m.aborted {
-		return exit.ErrAborted
+		return err
 	}
 
-	fmt.Println(m.textarea.Value())
+	fmt.Println(value)
 	return nil
 }
