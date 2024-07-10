f159a0b1aff502abddd2ab2a7a4ae6ec570b0834
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-07-10 11:24:07 -0400

    refactor: use x/ansi instead of regexp to strip ansi

--- ansi/ansi.go
+++ ansi/ansi.go
@@ -1,10 +1,8 @@
 package ansi
 
-import "regexp"
-
-var ansiEscape = regexp.MustCompile(`\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])`)
+import "github.com/charmbracelet/x/ansi"
 
 // Strip strips a string of any of it's ansi sequences.
 func Strip(text string) string {
-	return ansiEscape.ReplaceAllString(text, "")
+	return ansi.Strip(text)
 }
