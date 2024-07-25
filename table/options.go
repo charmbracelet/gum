4646d391feffda2d57a2a5c29730421d9a2a6196
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-07-25 16:01:52 -0400

    Update table/options.go

--- table/options.go
+++ table/options.go
@@ -16,6 +16,5 @@ type Options struct {
 	CellStyle     style.Styles `embed:"" prefix:"cell." envprefix:"GUM_TABLE_CELL_"`
 	HeaderStyle   style.Styles `embed:"" prefix:"header." envprefix:"GUM_TABLE_HEADER_"`
 	SelectedStyle style.Styles `embed:"" prefix:"selected." set:"defaultForeground=212" envprefix:"GUM_TABLE_SELECTED_"`
-	File          string       `short:"f" help:"file path" default:""`
 	ReturnColumn  int          `short:"r" help:"Which column number should be returned instead of whole row as string. Default=0 returns whole Row" default:"0"`
 }
