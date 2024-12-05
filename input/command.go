5223cc37f53f4451a8ef765abf95723343fdf674
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-12-05 10:31:35 -0300

    revert: feat: huh for gum input (#524)

--- input/command.go
+++ input/command.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/huh"
-	"github.com/charmbracelet/lipgloss"
 
+	"github.com/charmbracelet/gum/cursor"
 	"github.com/charmbracelet/gum/internal/exit"
 	"github.com/charmbracelet/gum/internal/stdin"
 )
@@ -16,52 +15,52 @@ import (
 // Run provides a shell script interface for the text input bubble.
 // https://github.com/charmbracelet/bubbles/textinput
 func (o Options) Run() error {
-	var value string
+	if o.Value == "" {
+		if in, _ := stdin.Read(); in != "" {
+			o.Value = in
+		}
+	}
+
+	i := textinput.New()
 	if o.Value != "" {
-		value = o.Value
+		i.SetValue(o.Value)
 	} else if in, _ := stdin.Read(); in != "" {
-		value = in
+		i.SetValue(in)
 	}
+	i.Focus()
+	i.Prompt = o.Prompt
+	i.Placeholder = o.Placeholder
+	i.Width = o.Width
+	i.PromptStyle = o.PromptStyle.ToLipgloss()
+	i.PlaceholderStyle = o.PlaceholderStyle.ToLipgloss()
+	i.Cursor.Style = o.CursorStyle.ToLipgloss()
+	i.Cursor.SetMode(cursor.Modes[o.CursorMode])
+	i.CharLimit = o.CharLimit
 
-	theme := huh.ThemeCharm()
-	theme.Focused.Base = lipgloss.NewStyle()
-	theme.Focused.TextInput.Cursor = o.CursorStyle.ToLipgloss()
-	theme.Focused.TextInput.Placeholder = o.PlaceholderStyle.ToLipgloss()
-	theme.Focused.TextInput.Prompt = o.PromptStyle.ToLipgloss()
-	theme.Focused.Title = o.HeaderStyle.ToLipgloss()
-
-	// Keep input keymap backwards compatible
-	keymap := huh.NewDefaultKeyMap()
-	keymap.Quit = key.NewBinding(key.WithKeys("ctrl+c", "esc"))
-
-	echoMode := huh.EchoModeNormal
 	if o.Password {
-		echoMode = huh.EchoModePassword
+		i.EchoMode = textinput.EchoPassword
+		i.EchoCharacter = '•'
 	}
 
-	err := huh.NewForm(
-		huh.NewGroup(
-			huh.NewInput().
-				Prompt(o.Prompt).
-				Placeholder(o.Placeholder).
-				CharLimit(o.CharLimit).
-				EchoMode(echoMode).
-				Title(o.Header).
-				Value(&value),
-		),
-	).
-		WithShowHelp(false).
-		WithWidth(o.Width).
-		WithTheme(theme).
-		WithKeyMap(keymap).
-		WithTimeout(o.Timeout).
-		WithShowHelp(o.ShowHelp).
-		WithProgramOptions(tea.WithOutput(os.Stderr)).
-		Run()
+	p := tea.NewProgram(model{
+		textinput:   i,
+		aborted:     false,
+		header:      o.Header,
+		headerStyle: o.HeaderStyle.ToLipgloss(),
+		timeout:     o.Timeout,
+		hasTimeout:  o.Timeout > 0,
+		autoWidth:   o.Width < 1,
+	}, tea.WithOutput(os.Stderr))
+	tm, err := p.Run()
 	if err != nil {
-		return exit.Handle(err, o.Timeout)
+		return fmt.Errorf("failed to run input: %w", err)
+	}
+	m := tm.(model)
+
+	if m.aborted {
+		return exit.ErrAborted
 	}
 
-	fmt.Println(value)
+	fmt.Println(m.textinput.Value())
 	return nil
 }
---
+++ input/input.go
@@ -0,0 +1,78 @@
+// Package input provides a shell script interface for the text input bubble.
+// https://github.com/charmbracelet/bubbles/tree/master/textinput
+//
+// It can be used to prompt the user for some input. The text the user entered
+// will be sent to stdout.
+//
+// $ gum input --placeholder "What's your favorite gum?" > answer.text
+package input
+
+import (
+	"time"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/gum/timeout"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type model struct {
+	autoWidth   bool
+	header      string
+	headerStyle lipgloss.Style
+	textinput   textinput.Model
+	quitting    bool
+	aborted     bool
+	timeout     time.Duration
+	hasTimeout  bool
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(
+		textinput.Blink,
+		timeout.Init(m.timeout, nil),
+	)
+}
+
+func (m model) View() string {
+	if m.quitting {
+		return ""
+	}
+	if m.header != "" {
+		header := m.headerStyle.Render(m.header)
+		return lipgloss.JoinVertical(lipgloss.Left, header, m.textinput.View())
+	}
+
+	return m.textinput.View()
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case timeout.TickTimeoutMsg:
+		if msg.TimeoutValue <= 0 {
+			m.quitting = true
+			m.aborted = true
+			return m, tea.Quit
+		}
+		m.timeout = msg.TimeoutValue
+		return m, timeout.Tick(msg.TimeoutValue, msg.Data)
+	case tea.WindowSizeMsg:
+		if m.autoWidth {
+			m.textinput.Width = msg.Width - lipgloss.Width(m.textinput.Prompt) - 1
+		}
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			m.quitting = true
+			m.aborted = true
+			return m, tea.Quit
+		case "enter":
+			m.quitting = true
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	m.textinput, cmd = m.textinput.Update(msg)
+	return m, cmd
+}
