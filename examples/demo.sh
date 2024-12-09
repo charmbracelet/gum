4160854a6a8b5f0c8e11759b377efd713d3af9f6
bashbunni <bunni@bashbunni.dev>
2024-12-09 08:52:11 -0800

    fix(examples): remove -e from echo

--- examples/demo.sh
+++ examples/demo.sh
@@ -3,11 +3,11 @@
 gum style --border normal --margin "1" --padding "1 2" --border-foreground 212 "Hello, there! Welcome to $(gum style --foreground 212 'Gum')."
 NAME=$(gum input --placeholder "What is your name?")
 
-echo -e "Well, it is nice to meet you, $(gum style --foreground 212 "$NAME")."
+echo "Well, it is nice to meet you, $(gum style --foreground 212 "$NAME")."
 
 sleep 1; clear
 
-echo -e "Can you tell me a $(gum style --italic --foreground 99 'secret')?\n"
+echo "Can you tell me a $(gum style --italic --foreground 99 'secret')?\n"
 
 gum write --placeholder "I'll keep it to myself, I promise!" > /dev/null # we keep the secret to ourselves
 
@@ -24,7 +24,7 @@ grep -q "$DISCARD" <<< "$ACTIONS" && gum spin -s monkey --title " Discarding you
 
 sleep 1; clear
 
-GUM=$(echo -e "Cherry\nGrape\nLime\nOrange" | gum filter --placeholder "Favorite flavor?")
+GUM=$(echo "Cherry\nGrape\nLime\nOrange" | gum filter --placeholder "Favorite flavor?")
 echo "I'll keep that in mind!"
 
 sleep 1; clear
