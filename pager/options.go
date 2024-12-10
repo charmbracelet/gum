c25be3c8c3f299a29094461f890296feacce4383
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-12-09 22:16:58 -0300

    feat(pager): make --soft-wrap the default
    closes #744

--- pager/options.go
+++ pager/options.go
@@ -13,7 +13,7 @@ type Options struct {
 	Content             string        `arg:"" optional:"" help:"Display content to scroll"`
 	ShowLineNumbers     bool          `help:"Show line numbers" default:"true"`
 	LineNumberStyle     style.Styles  `embed:"" prefix:"line-number." help:"Style the line numbers" set:"defaultForeground=237" envprefix:"GUM_PAGER_LINE_NUMBER_"`
-	SoftWrap            bool          `help:"Soft wrap lines" default:"false"`
+	SoftWrap            bool          `help:"Soft wrap lines" default:"true" negatable:""`
 	MatchStyle          style.Styles  `embed:"" prefix:"match." help:"Style the matched text" set:"defaultForeground=212" set:"defaultBold=true" envprefix:"GUM_PAGER_MATCH_"`                                                      //nolint:staticcheck
 	MatchHighlightStyle style.Styles  `embed:"" prefix:"match-highlight." help:"Style the matched highlight text" set:"defaultForeground=235" set:"defaultBackground=225" set:"defaultBold=true" envprefix:"GUM_PAGER_MATCH_HIGH_"` //nolint:staticcheck
 	Timeout             time.Duration `help:"Timeout until command exits" default:"0s" env:"GUM_PAGER_TIMEOUT"`
