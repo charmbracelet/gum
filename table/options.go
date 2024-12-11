a8cce1cad945972d2a066b5646776243b3080476
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-12-10 21:18:13 -0300

    feat(table): --lazy-quotes and --fields-per-record (#759)
    As per [csv.Reader].
    
    closes #345

--- table/options.go
+++ table/options.go
@@ -8,14 +8,16 @@ import (
 
 // Options is the customization options for the table command.
 type Options struct {
-	Separator string   `short:"s" help:"Row separator" default:","`
-	Columns   []string `short:"c" help:"Column names"`
-	Widths    []int    `short:"w" help:"Column widths"`
-	Height    int      `help:"Table height" default:"0"`
-	Print     bool     `short:"p" help:"static print" default:"false"`
-	File      string   `short:"f" help:"file path" default:""`
-	Border    string   `short:"b" help:"border style" default:"rounded" enum:"rounded,thick,normal,hidden,double,none"`
-	ShowHelp  bool     `help:"Show help keybinds" default:"true" negatable:"" env:"GUM_TABLE_SHOW_HELP"`
+	Separator       string   `short:"s" help:"Row separator" default:","`
+	Columns         []string `short:"c" help:"Column names"`
+	Widths          []int    `short:"w" help:"Column widths"`
+	Height          int      `help:"Table height" default:"0"`
+	Print           bool     `short:"p" help:"static print" default:"false"`
+	File            string   `short:"f" help:"file path" default:""`
+	Border          string   `short:"b" help:"border style" default:"rounded" enum:"rounded,thick,normal,hidden,double,none"`
+	ShowHelp        bool     `help:"Show help keybinds" default:"true" negatable:"" env:"GUM_TABLE_SHOW_HELP"`
+	LazyQuotes      bool     `help:"If LazyQuotes is true, a quote may appear in an unquoted field and a non-doubled quote may appear in a quoted field" default:"false" env:"GUM_TABLE_LAZY_QUOTES"`
+	FieldsPerRecord int      `help:"Sets the number of expected fields per record" default:"0" env:"GUM_TABLE_FIELDS_PER_RECORD"`
 
 	BorderStyle   style.Styles  `embed:"" prefix:"border." envprefix:"GUM_TABLE_BORDER_"`
 	CellStyle     style.Styles  `embed:"" prefix:"cell." envprefix:"GUM_TABLE_CELL_"`
