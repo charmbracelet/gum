e773e5e12a988c148d0d59f1c38cc9618ca6028e
Raphael Amorim <rapha850@gmail.com>
2025-02-04 11:43:42 -0300

    format code

--- table/command.go
+++ table/command.go
@@ -142,11 +142,11 @@ func (o Options) Run() error {
 	defer cancel()
 
 	m := model{
-		table:    table,
-		showHelp: o.ShowHelp,
+		table:         table,
+		showHelp:      o.ShowHelp,
 		hideIndicator: o.HideIndicator,
-		help:     help.New(),
-		keymap:   defaultKeymap(),
+		help:          help.New(),
+		keymap:        defaultKeymap(),
 	}
 	tm, err := tea.NewProgram(
 		m,
