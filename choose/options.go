501402cbbaf9e5a1b88055e5495228f107c0ed46
Sebastian Adamczyk <sadamczyk-gh@pm.me>
2025-06-14 15:38:25 +0200

    fix(choose): fix typo in environment variable `GUM_CCHOOSE_TIMEOUT` (#922)
    Co-authored-by: sadamczyk <13919759+sadamczyk@users.noreply.github.com>

--- choose/options.go
+++ choose/options.go
@@ -15,7 +15,7 @@ type Options struct {
 	Height           int           `help:"Height of the list" default:"10" env:"GUM_CHOOSE_HEIGHT"`
 	Cursor           string        `help:"Prefix to show on item that corresponds to the cursor position" default:"> " env:"GUM_CHOOSE_CURSOR"`
 	ShowHelp         bool          `help:"Show help keybinds" default:"true" negatable:"" env:"GUM_CHOOSE_SHOW_HELP"`
-	Timeout          time.Duration `help:"Timeout until choose returns selected element" default:"0s" env:"GUM_CCHOOSE_TIMEOUT"` // including timeout command options [Timeout,...]
+	Timeout          time.Duration `help:"Timeout until choose returns selected element" default:"0s" env:"GUM_CHOOSE_TIMEOUT"` // including timeout command options [Timeout,...]
 	Header           string        `help:"Header value" default:"Choose:" env:"GUM_CHOOSE_HEADER"`
 	CursorPrefix     string        `help:"Prefix to show on the cursor item (hidden if limit is 1)" default:"• " env:"GUM_CHOOSE_CURSOR_PREFIX"`
 	SelectedPrefix   string        `help:"Prefix to show on selected items (hidden if limit is 1)" default:"✓ " env:"GUM_CHOOSE_SELECTED_PREFIX"`
