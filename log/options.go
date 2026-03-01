dc7656156bb461ba621c86a6b4e93964b0ade2cb
Yash Khare <khareyash05@gmail.com>
2026-03-01 19:46:16 +0530

    feat: configuration file support for gum
    Signed-off-by: Yash Khare <khareyash05@gmail.com>

--- log/options.go
+++ log/options.go
@@ -11,12 +11,12 @@ type Options struct {
 	File       string `short:"o" help:"Log to file"`
 	Format     bool   `short:"f" help:"Format message using printf" xor:"format,structured"`
 	Formatter  string `help:"The log formatter to use" enum:"json,logfmt,text" default:"text"`
-	Level      string `short:"l" help:"The log level to use" enum:"none,debug,info,warn,error,fatal" default:"none"`
+	Level      string `short:"l" help:"The log level to use" enum:"none,debug,info,warn,error,fatal" default:"none" env:"GUM_LOG_LEVEL"`
 	Prefix     string `help:"Prefix to print before the message"`
 	Structured bool   `short:"s" help:"Use structured logging" xor:"format,structured"`
-	Time       string `short:"t" help:"The time format to use (kitchen, layout, ansic, rfc822, etc...)" default:""`
+	Time       string `short:"t" help:"The time format to use (kitchen, layout, ansic, rfc822, etc...)" default:"" env:"GUM_LOG_TIME"`
 
-	MinLevel string `help:"Minimal level to show" default:"" env:"GUM_LOG_LEVEL"`
+	MinLevel string `help:"Minimal level to show" default:"" env:"GUM_LOG_MIN_LEVEL"`
 
 	LevelStyle     style.Styles `embed:"" prefix:"level." help:"The style of the level being used" set:"defaultBold=true" envprefix:"GUM_LOG_LEVEL_"`
 	TimeStyle      style.Styles `embed:"" prefix:"time." help:"The style of the time" envprefix:"GUM_LOG_TIME_"`
